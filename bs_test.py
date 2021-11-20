bbc82f886d567c7eb8521607cf2c83bdfdc1e772
Mira <najafi.mehraneh@gmail.com>
2021-11-20 21:22:40 +0330

    Extracting data from posts and saving them to a csv file

--- bs_test.py
+++ bs_test.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd, numpy as np
 from pandas.io.json import json_normalize
+import time
 
 
 def parse_data(data):
@@ -18,32 +19,59 @@ def parse_data(data):
 
 
 def create_df(s: requests.session(), links: []):
-    information = pd.DataFrame()
+    information = pd.DataFrame(columns=['Account', 'Post Link', 'Caption', 'Post Type', 'Comments', 'View', 'Video Duration'])
     for i in range(len(links)):
+        time.sleep(1)
+        print(links[i])
         """
             Some links might fail, we will put the whole process in a try-catch
         """
-        try:
-            post = s.get(links[i])
-            post_data = bs(post, 'html.parser')
-            body = post_data.find('body')
-            script = body.find('script')
-            raw = script.text.strip().replace('window._sharedData =', '').replace(';', '')
-            json_data = json.loads(raw)
-            posts = json_data['entry_data']['PostPage'][0]['graphql']
-            posts = json.dumps(posts)
-            posts = json.loads(posts)
-            x = pd.DataFrame.from_dict(json_normalize(posts), orient='columns')
-            x.columns = x.columns.str.replace("shortcode_media.", "")
-            print("here" , x)
-            information.append(x)
+        for j in range(10):
+            try:
+                post = s.get(links[i])
+                post_data = bs(post.text, 'html.parser')
+                post_title = post_data.title.text
+                username = post_title.split()[0]
+                body = post_data.find('body')
+                scripts = body.find_all('script')
 
-        except Exception as e:
-            print(e)
+                for element in scripts:
+                    if 'window.__additionalDataLoaded' in element.text:
+                        post_script = element.text
+                        break
 
-    information = information.drop_duplicates(subset='shortcode')
-    information.index = range(len(information.index))
-    information.to_csv("nike_data.csv")
+                for k in range(len(post_script)):
+                    if post_script[k] == '{':
+                        json_text = post_script[k:-2]
+                        break
+                
+                print(json_text)
+                json_data = json.loads(json_text)
+                if json_data['graphql']['shortcode_media']['__typename'] == 'GraphImage':
+                    post_type = 'image'
+                    view = None
+                    video_duration = 0
+                    comments = json_data['graphql']['shortcode_media']['edge_media_to_parent_comment']
+                elif json_data['graphql']['shortcode_media']['__typename'] == 'GraphVideo':
+                    post_type = 'video'
+                    view = json_data['graphql']['shortcode_media']['video_view_count']
+                    video_duration = json_data['graphql']['shortcode_media']['video_duration']
+                    comments = json_data['graphql']['shortcode_media']['edge_media_to_parent_comment']
+                else:
+                    continue
+
+                row = pd.DataFrame({"Account": username, "Post Link": links[i], "Caption": post_title, "Post Type": post_type, "Comments": comments,
+                                    "View" : view, "Video Duration": video_duration})
+                print("ROW: ", row)
+                information = information.append(row, ignore_index=True)
+                break
+            except Exception as e:
+                print(e)
+                print("retying in 10 seconds...")
+                time.sleep(10)
+    #information = information.drop_duplicates(subset='shortcode')
+    #information.index = range(len(information.index))
+    information.to_csv(r"{}.csv".format(username))
 
 
 def login(username, password):
@@ -52,8 +80,17 @@ def login(username, password):
         login_url = 'https://www.instagram.com/accounts/login/ajax/'
 
         time = int(datetime.datetime.now().timestamp())
-        response = s.get(link)
-        csrf = response.cookies['csrftoken']
+
+        for i in range(5):
+            try:
+                response = s.get(link)
+                csrf = response.cookies['csrftoken']
+                break
+            except Exception as e:
+                print("retry...")
+
+        """response = s.get(link)
+        csrf = response.cookies['csrftoken']"""
         print(csrf)
         payload = {
             'username': username,
@@ -68,9 +105,13 @@ def login(username, password):
             "Referer": "https://www.instagram.com/accounts/login/",
             "x-csrftoken": csrf
         }
-
-        login_response = s.post(login_url, data=payload, headers=login_header)
-        json_data = json.loads(login_response.text)
+        for i in range(5):
+            try:
+                login_response = s.post(login_url, data=payload, headers=login_header)
+                json_data = json.loads(login_response.text)
+                break
+            except Exception as e:
+                print("trying..")
 
         if json_data["authenticated"]:
             print("login successful")
@@ -83,7 +124,7 @@ def login(username, password):
         else:
             print("login failed ", login_response.text)
 
-        url = "https://www.instagram.com/{}".format("nike")
+        url = "https://www.instagram.com/{}".format("adidas")
         data_received = s.get(url)
         bs_data = bs(data_received.text, "html.parser")
         title = bs_data.title
@@ -96,7 +137,6 @@ def login(username, password):
         create_df(s, links)
 
 
-
-#username = input("Enter your username: ")
-#password = input("Enter your password: ")
+# username = input("Enter your username: ")
+# password = input("Enter your password: ")
 login("mehranehn", "Passinsta174*")
